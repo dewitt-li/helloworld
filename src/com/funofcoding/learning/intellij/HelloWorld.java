911b2172d68560e14e49620bedcd305487137594
wei <wli@lapis.com>
2016-09-07 11:25:31 -0400

    initial commit

---
+++ src/com/funofcoding/learning/intellij/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.funofcoding.learning.intellij;
+
+/**
+ * Created by bella on 9/7/16.
+ */
+public class HelloWorld {
+    public static void main(String[] strs){
+        System.out.println("Hello World");
+    }
+}
